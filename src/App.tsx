4d4c1035a76c54418893d3b1dcdf2a06ad485c30
constantindjonkam <fokouaconstantin@gmail.com>
2023-04-08 12:47:24 -0400

    added routes for the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import OnBoarding from "./pages/OnBoarding";
+
 function App() {
-  return <div className="App">Dashboard</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/onboarding" element={<OnBoarding />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
