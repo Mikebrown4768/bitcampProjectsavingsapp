9aef7afe19a675c19e0fba6fd68a90a4c7f2bc5d
constantindjonkam <fokouaconstantin@gmail.com>
2023-04-08 12:47:50 -0400

    created Dashboard, login and onboarding components

---
+++ src/pages/Login.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface LoginProps {}
+
+const Login: React.FC<LoginProps> = ({}) => {
+  return (
+    <div>
+      <h1>Login</h1>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/pages/OnBoarding.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface OnBoardingProps {}
+
+const OnBoarding: React.FC<OnBoardingProps> = ({}) => {
+  return (
+    <div>
+      <h1>OnBoarding</h1>
+    </div>
+  );
+};
+
+export default OnBoarding;
