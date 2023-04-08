6fd19224ba25ce752f0d46477e8cc4093f98282c
Mikebrown4768 <mikebrown4768@gmail.com>
2023-04-08 14:20:05 -0400

    Added UL that holds the left bar options

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -4,9 +4,19 @@ interface DashboardProps {}
 
 const Dashboard: React.FC<DashboardProps> = ({}) => {
   return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
+      <>
+          <div>
+              <h6>Dashboard</h6>
+          </div>
+
+          <ul>
+              <li>Bank</li>
+              <li>Investments</li>
+              <li>Settings</li>
+          </ul>
+
+      </>
+
   );
 };
 
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,7 +6,12 @@ interface LoginProps {}
 const Login: React.FC<LoginProps> = ({}) => {
   return (
     <div>
+
       <h1>Login</h1>
+
+
+
+
     </div>
   );
 };
