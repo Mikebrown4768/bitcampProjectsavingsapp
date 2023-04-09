88cd2c8a87beb87cac4d61507d0a112a516a6344
constantindjonkam <fokouaconstantin@gmail.com>
2023-04-08 22:52:51 -0400

    added progress and past transactions section

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+import { FC } from "react";
 import BankCard from "../components/cards/BankCard";
 import IncomeCard from "../components/cards/IncomeCard";
+import useDashboard from "../hooks/useDashboard";
+import ProgressCard from "../components/cards/ProgressCard";
+import TransactionHistory from "../components/TransactionHistory";
 
 interface DashboardProps {}
 
-const Dashboard: React.FC<DashboardProps> = ({}) => {
+const Dashboard: FC<DashboardProps> = ({}) => {
+  useDashboard();
+
   return (
     <div className="p-6 flex-[0.8] bg-[#f3f3f3]">
       <div className="flex items-stretch gap-4">
@@ -12,6 +17,12 @@ const Dashboard: React.FC<DashboardProps> = ({}) => {
         <IncomeCard title="Income" price={62245.89} percentage={+12.23} />
         <IncomeCard title="Expense" price={70268.67} percentage={-15.73} />
       </div>
+
+      <div className="flex items-stretch gap-4 my-8">
+        <ProgressCard />
+      </div>
+
+      <TransactionHistory />
     </div>
   );
 };
