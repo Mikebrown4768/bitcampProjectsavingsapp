ac98948ea42512930918e2ce5f6951eda9601b9a
constantindjonkam <fokouaconstantin@gmail.com>
2023-04-08 22:49:04 -0400

    implemented the login and logout functionality of firebase google auth

--- src/utilities/firebaseAuth.ts
+++ src/utilities/firebaseAuth.ts
@@ -1,3 +1,17 @@
-export const login = () => {
-  console.log("login");
+import { auth } from "./../firebase";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+export const login = async () => {
+  const provider = new GoogleAuthProvider();
+  provider.setCustomParameters({ prompt: "select_account" });
+
+  const { user } = await signInWithPopup(auth, provider);
+
+  if (user) return user;
+
+  return null;
+};
+
+export const logout = async () => {
+  await auth.signOut();
 };
