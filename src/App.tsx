7fb22351c755e0c4a05c1c24497764ef29c45a87
constantindjonkam <fokouaconstantin@gmail.com>
2023-04-08 22:49:53 -0400

    displayed left nav for profile dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import OnBoarding from "./pages/OnBoarding";
-import Main from "./pages/Main";
+import LeftNav from "./components/LeftNav";
+import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/Profile";
+import useUser from "./hooks/useUser";
 
 function App() {
+  const { user, isLoading } = useUser();
+
+  if (isLoading) return null;
+
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/onboarding" element={<OnBoarding />} />
-      </Routes>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="flex">
+          <LeftNav />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/onboarding" element={<OnBoarding />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </div>
+      )}
     </Router>
   );
 }
